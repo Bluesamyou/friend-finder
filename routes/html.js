6065c51ca928f2513d0e63ebfccada5de464c897
Shevi <shevids@gmail.com>
2019-09-14 21:50:39 +1000

    Testing new route locations

--- routes/html.js
+++ routes/html.js
@@ -3,15 +3,15 @@ const express = require('express')
 var router = express.Router()
 
 router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/../pages/index.html'))
+    res.sendFile(path.join(__dirname + '/pages/index.html'))
 })
 
 router.get('/survey', function (req, res) {
-    res.sendFile(path.join(__dirname + '/../pages/survey.html'))
+    res.sendFile(path.join(__dirname + '/pages/survey.html'))
 })
 
 router.get('/styles/styles.css', function (req, res) {
-    res.sendFile(path.join(__dirname + '/../pages/styles/styles.css'))
+    res.sendFile(path.join(__dirname + '/pages/styles/styles.css'))
 })
 
 module.exports = router;
